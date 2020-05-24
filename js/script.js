375de8b8df1891a786523c7d0ac6a1a63daa889f
Steve Park <ghkfkd0323@gmail.com>
2020-05-23 22:59:38 -0700

    finished project

--- js/script.js
+++ js/script.js
@@ -3,57 +3,73 @@ const students = document.querySelectorAll('.student-item');
 const listDiv = document.querySelector('.student-list');
 const pageDiv = document.querySelector('.page');
 
-// get all students
-let allStudents = students;
-// FOR SEARCH FORM DISPLAY PURPOSE
-const pageHeader = document.querySelector('.page-header');
-const search = document.createElement('div');
-search.className = "student-search";
-const input = document.createElement('input');
-input.setAttribute('placeholder', "Search for students...");
-const button = document.createElement('button');
-button.textContent = "Search";
-search.appendChild(input);
-search.appendChild(button);
-pageHeader.appendChild(search);
+// MAKING SEARCH BOX
+function searchBox() {
+   
+   // FOR SEARCH FORM DISPLAY PURPOSE
+   const pageHeader = document.querySelector('.page-header');
+   const search = document.createElement('div');
+   search.className = "student-search";
 
-document.querySelector('input[placeholder]').addEventListener('input', (e) => {
-   // console.log(e.target.value, 'hi')
+   // INPUT
+   const input = document.createElement('input');
+   input.setAttribute('placeholder', "Search for students...");
 
+   // SUBMIT BUTTON
+   const button = document.createElement('button');
+   button.textContent = "Search";
+
+   // APPENDING TO SEARCH DIV
+   search.appendChild(input);
+   search.appendChild(button);
+   pageHeader.appendChild(search);
+   // FOR SEARCH FORM DISPLAY PURPOSE
+
+   input.addEventListener('keyup', searchingInputValue);
+   button.addEventListener('click', searchingInputValue);
+}
+
+
+function searchingInputValue() {
    const name = document.getElementsByTagName('h3');
-   // console.log(name)
-   const inputValue = e.target.value;
-    // LOOPING THROUGH EACH ANCHOR TAG 'DATA-TITLE' ATTRIBUTE IN GALLERY DIV
-    allStudents.forEach( (element, i) => {
+   const inputValue = document.querySelector('input[placeholder]').value.toLowerCase();
+   console.log(name, 'name check')
+   console.log(inputValue)
+   let searchedList = []
 
-      console.log(name[i].innerText);
+   if(!inputValue) {
+      pageOnload(students)
+      return;
+   }
 
-      // console.log(element.children[0].childNodes[3])
-        // PARAMETER ELEMENT IS WHOLE ANCHOR TAG IN HTML
-        // GET DATA TITLE( CAPTION )  FROM ANCHOR TAG
-      //   let name = element.getElementsByTagName('h3').toLowerCase();
-        
-      //   // IF TITLE DOES NOT INCLUDE INPUT VALUE SET DISPLAY NONE
-      //   if( !title.includes(inputValue.toLowerCase()) ) {
-      //       element.parentElement.style.display = "none"
-            
-      //       // UNSET GROUP OF IMAGES FROM LIGHTBOX PLUGIN
-      //       // THIS MAKES ONLY INCLUDE INPUT VALUE IMAGES TO VISIBLE ON THE LIGHTBOX 
-      //       element.setAttribute('data-lightbox', "")
-      //   }
-      //   else { // IF TITLE DOES INCLUDE INPUT VALUE   
-      //       element.parentElement.style.display = "block"
-      //   }
-    });
-})
+   if(document.querySelector('.message')) {
+      document.querySelector('.message').remove()
+
+   }
+   students.forEach( (element, i) => {
+      // console.log(name[i].innerText.toLowerCase());
+      // console.log(element.childNodes[1].childNodes[3].textContent)
+      // console.log(name[i].parentNode.parentNode)
+
+      if(name) {
+         if(name[i].innerText.toLowerCase().includes(inputValue)){
+            // console.log(name[i].parentNode.parentNode)
+            name[i].parentNode.parentNode.style.display = ''
+            searchedList.push(element);
+         } else {
+            name[i].parentNode.parentNode.style.display = 'none'
+         }
+
+      }
+   });
+
+   console.log(searchedList, 'result');
+   pageOnload(searchedList)
+}
 
 
 // STUDENT LISTS TO WEBPAGE
 const showPage = (list, page) => {
-
-   // IF JAVASCRIPT WORKS, THAN HIDE HTML MARKUP
-   listDiv.innerHTML = "";
-
    // 10 LISTS STUDENTS FROM STUDENT LIST ON PAGE'S NUMBER
    let startIndex = ( page * 10 ) - 10;
    let endIndex = page * 10;
@@ -62,27 +78,34 @@ const showPage = (list, page) => {
    if( endIndex > list.length) {
       endIndex = list.length;
    }
-
+   
    // ADD STUDENTS TO UNORDER LIST
-   for( let i = startIndex; i < endIndex; i++) {
-      listDiv.appendChild(list[i]);
+   for (let i = 0; i < list.length; i++) {
+      if ( !(i >= startIndex && i < endIndex) ) {
+         list[i].style.display = 'none';
+      } else {
+         list[i].style.display = ''
+      }
    }
 }
+
 
 // PAGINATION LINKS
 const appendPageLinks = list => {
 
-   // IF THERE IS NO STUDENT, SHOW THE MESSAGE
-   if( list.length === 0 ) {
-      return listDiv.innerHTML = `
-         <li class="student-item cf">There is no student</li>
-      `;
+   // REMOVE PREVIOUS PAGINATION LINKS
+   if(document.querySelector('.pagination')) {
+      document.querySelector('.pagination').remove();
    }
 
-   // GET HOW MANY PAGES NEED
-   let pageNumbers = Math.ceil(list.length / 10);
-   if (list.length % 10 !== 0) {
-      pageNumbers += 1;
+   // MESSAGE BOX
+   const noMatchMessage = document.createElement('li');
+   noMatchMessage.className = 'message student-item';
+   noMatchMessage.innerText = 'There is no matched student.'
+
+   // IF THERE IS NO STUDENT, SHOW THE MESSAGE
+   if( list.length === 0 ) {
+      return listDiv.appendChild(noMatchMessage);
    }
 
    // CREATE PAGINATION DIV
@@ -90,6 +113,12 @@ const appendPageLinks = list => {
    const ul = document.createElement('ul');
    pagination.className = 'pagination';
    pagination.appendChild(ul);
+   
+   // GET HOW MANY PAGES NEED
+   let pageNumbers = Math.ceil(list.length / 10);
+   if (list.length % 10 !== 0) {
+      pageNumbers += 1;
+   }
 
    // CREATE PAGINATION LINKS TO ul
    for( let i = 1; i < pageNumbers; i++) {
@@ -103,33 +132,53 @@ const appendPageLinks = list => {
 
    // APPEND PAGINATION LINKS TO BOTTOM OF THE WEBPAGE
    pageDiv.appendChild(pagination);
-   showPage(students, 1)
+   pageLinksEvent(list);
+}
 
 
-   // THIS ATTRIBUTE SELECTOR WILL GET FIRST ELEMENT 
-   // AND SET CLASSNAME ACTIVE
+// PAGINATION LINK EVENT LISTENER
+const pageLinksEvent = list => {
+   // THIS ATTRIBUTE SELECTOR WILL GET FIRST ELEMENT AND SET CLASSNAME ACTIVE
    document.querySelector('a[href="#"]').className = 'active';
 
-   // PAGINATION LINK EVENT LISTENER
-   // LOOP THROUGH EACH LINK ELEMENT AND WAIT FOR USER CLICK
-   document.querySelectorAll('a[href="#"]').forEach( (event, i) => {
+   // GET ALL ANCHOR TAG THAT HAS ATTRIBUTE 'href=#'
+   document.querySelectorAll('a[href="#"]').forEach( event => {
+
+      // LOOP THROUGH EACH LINK ELEMENT AND WAIT FOR USER CLICK
       event.addEventListener('click', e => {
          console.log(e.target.textContent, 'page number')
-         showPage(students, e.target.textContent)
+         showPage(list, e.target.textContent);
          toggleActive(e.target);
       })
    })
 }
 
-appendPageLinks(students);
 
 // TOGGLE ACTIVE CLASS WHEN USER CLIKED PAGE NUMBER
-function toggleActive(event) {
+const toggleActive= event => {
    // GET CURRENT ACTIVE CLASS AND REMOVE
-   // console.log(event)
-   let current = document.getElementsByClassName("active")[0];
+   const current = document.getElementsByClassName("active")[0];
    current.classList.remove("active");
 
    // ACTIVE CLASS ON CLICKED ELEMENT
    event.classList.add("active");
 }
+
+
+// EXECUTE IT WHEN THE WEBPAGE LOAD.
+// GET ALL STUDENTS AND HIDE IN THE HTML MARKUP
+const pageOnload = list => {
+   console.log(list, 'pageonload list');
+   // FIRST HIDE ALL STUDENTS
+   students.forEach( e => {
+      e.style.display = 'none';
+   });
+
+   // SHOW FIRST PAGE WHEN THE WEBPAGE LOAD
+   showPage(list, 1);
+   appendPageLinks(list);
+}
+
+
+pageOnload(students);
+searchBox();
